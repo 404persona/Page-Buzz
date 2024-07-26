f02dde3257589f1ece50e1d4d29e26e919163016
anastanvir <anas.seoalchemist@gmail.com>
2024-07-25 21:47:12 -0700

    Update

---
+++ Server/Server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const Server = express()
+require('dotenv').config()
+const colors = require('colors')
+
+PORT = process.env.PORT || 3000
+
+Server.listen(PORT, ()=>{
+    console.log(`Server is tunned on port at ${PORT}`.bgWhite.bold)
+})
