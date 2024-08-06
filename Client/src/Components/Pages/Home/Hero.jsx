a062fe219418c3b100d94deb34b06e4e7d8751b4
anastanvir <anas.seoalchemist@gmail.com>
2024-08-06 12:21:17 -0700

    Projects Details Completed and Responsive

--- Client/src/Components/Pages/Home/Hero.jsx
+++ Client/src/Components/Pages/Home/Hero.jsx
@@ -6,9 +6,9 @@ import { Herotext } from '../../../Data/Data';
 const Hero = () => {
   const textVariants = {
     hidden: { opacity: 0, y: 100 },
-    visible: { 
-      opacity: 1, 
-      y: 0, 
+    visible: {
+      opacity: 1,
+      y: 0,
       transition: {
         delay: 0.2,
         duration: .1,
